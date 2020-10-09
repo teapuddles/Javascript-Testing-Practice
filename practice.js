f0a0470533727b8fafc62aa8f4463d5a77ac5140
teapuddles <kingsley.gleeson@gmail.com>
2020-10-09 14:31:35 -0400

    adding lesson 2

--- practice.js
+++
@@ -1,7 +0,0 @@
-function practice (a, b) {
-    return a + b
-}
-
-console.log(practice(1,22))
-
-module.exports = practice 
---
+++ practice/practice.js
@@ -0,0 +1,5 @@
+function practice(a, b) {
+    return a + b
+}
+
+module.exports = practice 
